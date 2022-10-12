2286ff255bfac117b5c2fc791f24aaf708b47dc0
Joel Aguilar <joel3.1416@gmail.com>
2022-10-11 22:14:29 -0500

    profile init

--- src/auth/Profile.jsx
+++ src/auth/Profile.jsx
@@ -6,9 +6,15 @@ function Profile(props) {
     const { error, user, isLoading, isAuthenticated } = useAuth0();
 
     return (
+        isAuthenticated &&
         <div>
-            {user?.name || "Cargando..."}
-            {console.log(error, user, isLoading, isAuthenticated)}
+            <div>Nombre: {user.name}</div>
+            <div>Nick: {user.nickname}</div>
+            <div>Nombre Completo: {`${user.given_name} ${user.family_name}`}</div>
+            <div>PP: <img src={user.picture} /></div>
+            <div>Email: {user.email}</div>
+            {/* <div>Nombre: {user.name}</div> */}
+
             {JSON.stringify(user)}
         </div>
     );
