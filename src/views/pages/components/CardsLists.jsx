051fd4c47efbe22627a603e2d2794ae57c3b985d
Lucas0894 <luema94@gmail.com>
2022-10-17 21:10:19 -0300

    se realizo el paginado

--- src/views/pages/components/CardsLists.jsx
+++ src/views/pages/components/CardsLists.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
-import { Container, Row,Col, Navbar } from 'react-bootstrap'
+import { Container, Row,Col } from 'react-bootstrap'
 import CardFormat from './CardFormat';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getAllProfessionals } from '../../../state/ducks/professionals/actions';
 import SearchBar from './SearchBar';
+import Paginado from './Paginado';
+
 
 
 
 function CardsList() {
   const profesionales = useSelector((state) => (state.allProfessionals))
   const dispatch = useDispatch(); 
+    
+     const [currentPage, setCurrentPage] = useState(1);
+   
+     const professionalsPerPage = 8;
+    
+     const indexOfLastProfessional = currentPage * professionalsPerPage;
+    
+     const indexOfFirstProfessional = indexOfLastProfessional - professionalsPerPage;
+   
+     const currentProfessionals = profesionales.slice(indexOfFirstProfessional , indexOfLastProfessional)
+
+     const paginado = (pageNum) => {
+         setCurrentPage(pageNum)
+     }
   
+  <Paginado 
+  professionalsPerPage={professionalsPerPage}
+  profesionales ={profesionales.length}
+  paginado ={paginado}
+  currentPage={currentPage}
+  />
   
   useEffect(()=>{
     dispatch(getAllProfessionals())
 },[dispatch])
   
 
- 
-
   return (
-    
     <Container style={{width:'100%', justifyContent: "center", display: "flex", flexDirection: "column"}}>
       <SearchBar/>
         <Row style={{margin: "10px"}} xs={1} md={3} lg={4} className="g-4" >
-        {profesionales?.map((e)=>{
+        {currentProfessionals?.map((e)=>{
             return (<Col key={e.id}style={{display:'flex',justifyContent:'center'}}><CardFormat worker ={e} key={e.id}/></Col>)
         })}</Row>
     </Container>
