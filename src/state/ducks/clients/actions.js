e989eed3852153f990504d3102bebfe9f5005180
johnoibaf <jroafernandez@gmail.com>
2022-10-21 14:05:33 -0500

    componente registro cliente

--- src/state/ducks/clients/actions.js
+++ src/state/ducks/clients/actions.js
@@ -1,3 +1,16 @@
-import { createAction } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const getClient = createAction("GET_CLIENT"); // getClient a data lo toma como payload
+
+export function postClient(payload){
+    return async function(dispatch){
+        try {
+            const postProfessionals = await axios.post('/home/client', payload)
+            return dispatch({
+                type: 'POST_CLIENT',
+                postProfessionals
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
