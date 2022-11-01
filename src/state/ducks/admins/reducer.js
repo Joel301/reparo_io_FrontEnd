bddc237aff1e39882c5126afd8e281144cbe0728
LucasMarchetti <luksmarchetti@gmail.com>
2022-11-01 12:10:12 -0300

    se creo el redux para admins

---
+++ src/state/ducks/admins/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  
+  
+}
+
+
+const adminsReducer = (state = initialState, action) => {
+    switch (action.type) {
+      
+      default: return state;
+    }
+
+}
+
+export default adminsReducer; 
--- src/state/ducks/index.js
+++ src/state/ducks/index.js
@@ -3,12 +3,15 @@ import rootReducer from "./professionals/reducer";
 import detailReducer from "./detail/reducer";
 import clientReducer from "./clients/reducer";
 import cartReducer from "./cart/reducer";
+import adminsReducer from "./admins/reducer";
+
 
 const reducers = combineReducers({
   professionals: rootReducer,
   detail: detailReducer,
   clients: clientReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  admins: adminsReducer
 })
 
 export default reducers;
