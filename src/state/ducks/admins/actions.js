5f0e5e50097f68a059300940355b62ff710f6c45
armoraz <oawind@gmail.com>
2022-11-08 09:04:40 -0300

    agregada action para todas las ordenes

--- src/state/ducks/admins/actions.js
+++ src/state/ducks/admins/actions.js
@@ -1,64 +1,87 @@
 import axios from "axios";
 
 export function getClient(id) {
-    console.log(id, "id action")
-    return async function (dispatch) {
-        try {
-            let client = await axios.get(`https://reparoiobackend-main.up.railway.app/api/clients/${id}`)
-            console.log(client.data, "client action")
-            return dispatch ({
-                type: "GET_CLIENT",
-                payload: client.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  console.log(id, "id action");
+  return async function (dispatch) {
+    try {
+      let client = await axios.get(
+        `https://reparoiobackend-main.up.railway.app/api/clients/${id}`
+      );
+      console.log(client.data, "client action");
+      return dispatch({
+        type: "GET_CLIENT",
+        payload: client.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
-export function getProfessions () {
-    return async function (dispatch) {
-        try {
-            const profesiones = await axios.get(
-                `https://reparoiobackend-main.up.railway.app/home/professions/`
-              )
-              return dispatch({
-                type: 'GET_PROFESSIONS',
-                payload: profesiones.data
-              })
-        } catch (error) {
-            console.log(error)
-        }
+export function getProfessions() {
+  return async function (dispatch) {
+    try {
+      const profesiones = await axios.get(
+        `https://reparoiobackend-main.up.railway.app/home/professions/`
+      );
+      return dispatch({
+        type: "GET_PROFESSIONS",
+        payload: profesiones.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
-export function deleteProfession (id) {
-    return async function (dispatch) {
-        try {
-            await axios.delete(`https://reparoiobackend-main.up.railway.app/home/professions/${id}`)
-            return dispatch ({
-                type: "DELETE_PROFESION",
-                payload: {
-                    id
-                }
-            })
-        } catch (error) {
-            console.log(error)
-        }
+export function deleteProfession(id) {
+  return async function (dispatch) {
+    try {
+      await axios.delete(
+        `https://reparoiobackend-main.up.railway.app/home/professions/${id}`
+      );
+      return dispatch({
+        type: "DELETE_PROFESION",
+        payload: {
+          id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
-export function addProfessions (body) {
-    return async function (dispatch) {
-        try {
-            await axios.post("https://reparoiobackend-main.up.railway.app/home/professions", body)
-            return dispatch ({
-                type: "ADD_PROFESION",
-                payload: body
-            })
-        } catch (error) {
-            console.log(error)
-        }
+export function addProfessions(body) {
+  return async function (dispatch) {
+    try {
+      await axios.post(
+        "https://reparoiobackend-main.up.railway.app/home/professions",
+        body
+      );
+      return dispatch({
+        type: "ADD_PROFESION",
+        payload: body,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
+export function getAllOrders() {
+  return async function () {
+    const response = await axios.get(
+      "https://reparoiobackend-main.up.railway.app/api/orders"
+    );
+    //response.data = {orders, orderStatus}
+    //orderStatus incluye las posibles opciones de status
+    //orders = {
+    //id, amount, status, clientId, date, orderDetails:[professionalId, reservationAmount,days]
+    //}
+    return {
+      type: "GET_ORDERS",
+      payload: response.data,
+    };
+  };
+}
