5a4007ae02ec737f1f78fd54aecd0eefa0685d62
Hiram Gerez <hiramgnc@hotmail.com>
2022-11-09 02:26:30 -0300

    agrego estilos card

--- src/views/components/CardFormat.jsx
+++ src/views/components/CardFormat.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import style from "./CardFormat.module.css"
 import { Card, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../state/ducks/cart/actions";
@@ -21,31 +21,51 @@ function CardFormat({ worker,estado,estadoBoolean,setRepeatedAlert,repeatedAlert
    
     estadoBoolean(estado=true)
   }
-  return (
-    <Card bg="light" style={{ width: "18rem", height: "22rem" }}>
-      <div
-        variant="top"
-        
-        style={{ height: "10rem",width:'16.5rem',backgroundImage:`url(${worker.profileImg})`,  backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",borderRadius:'0.5rem',
-        backgroundPosition: "center"}}
-      />
-      <Card.Body>
-        <Card.Title>{`${worker.firstName} ${worker.lastName}`}</Card.Title>
 
-        <Card.Text>{profToString?.join(", ")}</Card.Text>
-        <Card.Subtitle>Reputacion: {worker.review}</Card.Subtitle>
-      </Card.Body>
-      <Card.Footer
-        style={{ display: "flex", gap: "2rem", justifyContent: "center" }}
-      >
-        <Button onClick={()=> reserveOnClick()}>Reservar</Button>
-        <Link to={`/detail/${worker.id}`}>
-          <Button bg="primary">Ver Mas...</Button>
-        </Link>
-      </Card.Footer>
-    </Card>
-  );
-}
+    return (
+      <Card class="card shadow" bg="light" style={{ width: "18rem", height: "22rem" }}>
+  
+        <div className={style.inner}>
+          
+        <div className={style.img}
+            variant="top"
+            
+            style={{ height: "10rem",width:'16.5rem',backgroundImage:`url(${worker.profileImg})`,  backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",borderRadius:'0.5rem',
+            backgroundPosition: "center"}}
+          />
+        </div>
+  
+        <Card.Body>
+          <Card.Title style={{display: "flex", gap: "2rem", justifyContent: "center", fontWeight: "bold", 
+          textTransform: "capitalize", textPrimary:"red"}}>
+            {`${worker.firstName} ${worker.lastName}`}
+          </Card.Title>
+  
+          <Card.Text class="text-secondary" style={{display: "flex", gap: "2rem", justifyContent: "center",
+          textTransform: "capitalize"}}>
+            {profToString?.join(", ")}
+          </Card.Text>
+  
+          <Card.Subtitle style={{display: "flex", gap: "2rem", justifyContent: "center", 
+          textTransform: "capitalize"}}>
+            Reputacion: {worker.review}
+          </Card.Subtitle>
+        </Card.Body>
+        <Card.Footer
+          style={{ display: "flex", gap: "2rem", justifyContent: "center" }}
+        >
+          <Link to={`/detail/${worker.id}`}>
+            <Button bg="primary">Ver Mas...</Button>
+          </Link>
+          
+          <Button variant="success"onClick={()=> reserveOnClick()}>
+            Reservar
+          </Button>
+          
+        </Card.Footer>
+      </Card>
+    );
+  }
 
 export default CardFormat;
